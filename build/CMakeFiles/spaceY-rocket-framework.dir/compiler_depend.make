9090630f0480009f448ac54adf55fc1d8d9f5355
spicymuffin <54279408+spicymuffin@users.noreply.github.com>
2023-10-13 19:32:49 +0900

    cmake....

---
+++ build/CMakeFiles/spaceY-rocket-framework.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for spaceY-rocket-framework.
+# This may be replaced when dependencies are built.
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/luigi/Desktop/github/spaceY-rocket-framework
+# Build directory: C:/Users/luigi/Desktop/github/spaceY-rocket-framework/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
